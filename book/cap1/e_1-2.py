911bd992b0bae726ddf721a024054657134f434b
Caio Dantas <caiodantasl.lara@gmail.com>
2023-11-07 17:18:33 -0300

    cap1

---
+++ book/cap1/e_1-2.py
@@ -0,0 +1,20 @@
+"""Inicialize o interpretador do Python e use-o como uma calculadora.
+
+    Q1 - Quantos segundos há em 42 minutos e 42 segundos?
+
+    Q2 - Quantas milhas há em 10 quilômetros? Dica: uma milha equivale a 1,61 quilômetro.
+
+    Q3 - Se você correr 10 quilômetros em 42 minutos e 42 segundos, qual é o seu passo médio (tempo por milha em minutos e segundos)? Qual é a sua velocidade média em milhas por hora?
+"""
+seconds = 42 * 60 + 42
+print(f'Q1 = {seconds}')
+
+
+miles = 10 / 1.61
+print(f'Q2 = {miles:.2f}')
+
+metros_in_miles = 999402.6
+v = metros_in_miles / seconds
+print(f'{v:.2f}')
+
+print(390.09 * seconds)
