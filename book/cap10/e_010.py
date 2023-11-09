2ea0fcbf10c7dc54c973feb0d1c28b0322a3ff13
Caio Dantas <caiodantasl.lara@gmail.com>
2023-11-09 17:33:28 -0300

    rrrr

---
+++ book/cap10/e_010.py
@@ -0,0 +1,20 @@
+"""
+[10, 20, 30, 40]
+['crunchy frog', 'ram bladder', 'lark vomit']
+
+['spam', 2.0, 5, [10, 20]]
+
+cheeses = ['Cheddar', 'Edam', 'Gouda']
+numbers = [42, 123]
+empty = []
+print(cheeses, numbers, empty)
+['Cheddar', 'Edam', 'Gouda'] [42, 123] []
+
+cheeses[0]
+'Cheddar'
+
+numbers = [42, 123]
+numbers[1] = 5
+numbers
+[42, 5]
+"""
